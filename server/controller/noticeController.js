49c6dcf1c1a50b981e5c6eebbecffc3147d9f773
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-02-18 15:29:30 +0530

    refactor: changed class controllers to functions and make descriptive function names

--- server/controller/noticeController.js
+++ server/controller/noticeController.js
@@ -1,54 +1,47 @@
-const NoticeModel = require("../models/notices");
+const NoticeModel = require('../models/notices');
 
-class Notice{
-    async post(req,res){
-        let {topic,title,description,body,isImportant} = req.body;
-        if(!topic || !title || !description){
-            return res.json({error: "Fields must not be empty"});
-        }
-        try{
-            const newNotice = new NoticeModel({topic,title,description,body,isImportant});
-            const savedNotice = await newNotice.save();
-            res.json({savedNotice});
-        }   
-        catch(err){
-            console.log(err);
-            res.json({res: "Internal error"});
-        }
+const postNotice = async (req, res) => {
+  let { topic, title, description, body, isImportant } = req.body;
+  if (!topic || !title || !description) {
+    return res.json({ error: 'Fields must not be empty' });
+  }
+  try {
+    const newNotice = new NoticeModel({ topic, title, description, body, isImportant });
+    const savedNotice = await newNotice.save();
+    res.json({ savedNotice });
+  } catch (err) {
+    console.log(err);
+    res.json({ res: 'Internal error' });
+  }
+};
+
+const getAllNotice = async (req, res) => {
+  try {
+    const allNotices = await NoticeModel.find();
+    res.json({ result: allNotices });
+  } catch (err) {
+    console.log(err);
+    res.json({ error: 'Internal error' });
+  }
+};
+
+const getOneNotice = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  if (!id) {
+    res.json({ error: 'id should not be empty' });
+  }
+  try {
+    const data = await NoticeModel.findOne({ _id: id });
+    if (!data) {
+      res.json({ error: 'No notice with this id exists' });
+    } else {
+      res.json(data);
     }
+  } catch (err) {
+    console.log(err);
+    res.json({ error: 'Internal server error' });
+  }
+};
 
-    async all(req,res){
-        try{
-            const allNotices = await NoticeModel.find();
-            res.json({result: allNotices});
-        }
-        catch(err){
-            console.log(err);
-            res.json({error: "Internal error"});
-        }
-    }
-
-    async singlepost(req,res){
-        const id = req.params.id;
-        console.log(id);
-        if(!id){
-            res.json({error: "id should not be empty"});
-        }
-        try{
-            const data = await NoticeModel.findOne({_id: id});
-            if(!data){
-                res.json({error: "No notice with this id exists"});
-            }
-            else{
-                res.json(data);
-            }
-        }
-        catch(err){
-            console.log(err);
-            res.json({error: "Internal server error"});
-        }
-    }
-}
-
-const noticeController = new Notice();
-module.exports = noticeController;
+module.exports = { postNotice, getAllNotice, getOneNotice };
