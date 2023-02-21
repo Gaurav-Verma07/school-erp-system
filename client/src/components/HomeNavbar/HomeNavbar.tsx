adc342936532d72616fcac07f385e7b321dd7ae2
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-02-22 00:29:44 +0530

    refactor: styles extracte

--- client/src/components/HomeNavbar/HomeNavbar.tsx
+++ client/src/components/HomeNavbar/HomeNavbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { createStyles, Navbar, Group, Code } from '@mantine/core';
+import { Navbar, Group, Code } from '@mantine/core';
 import {
   IconSettings,
   IconDatabaseImport,
@@ -15,59 +15,7 @@ import {
   IconUserPlus,
 } from '@tabler/icons';
 import { NavLink, useNavigate } from 'react-router-dom';
-
-const useStyles = createStyles((theme, _params, getRef) => {
-  const icon: any = getRef('icon');
-  return {
-    header: {
-      paddingBottom: theme.spacing.md,
-      marginBottom: theme.spacing.md * 1.5,
-    },
-
-    footer: {
-      paddingTop: theme.spacing.md,
-      marginTop: theme.spacing.md,
-      borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
-    },
-
-    link: {
-      ...theme.fn.focusStyles(),
-      display: 'flex',
-      alignItems: 'center',
-      textDecoration: 'none',
-      fontSize: theme.fontSizes.sm,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-      borderRadius: theme.radius.sm,
-      fontWeight: 500,
-
-      '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-        [`& .${icon}`]: {
-          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        },
-      },
-    },
-
-    linkIcon: {
-      ref: icon,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
-      marginRight: theme.spacing.sm,
-    },
-
-    linkActive: {
-      '&, &:hover': {
-        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-        [`& .${icon}`]: {
-          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-        },
-      },
-    },
-  };
-});
+import useStyles from './styles';
 
 const data = [
   { link: 'home', label: 'Home', icon: IconHome2 },
