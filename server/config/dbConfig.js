096e8a0171eab8febcd861f1772779c024ff691f
SHAIKASIFALI <12006051@svuceedu.onmicrosoft.com>
2023-02-22 13:25:21 +0530

    restored dbConfig.js file

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(
-    "mongodb+srv://itsmeshaikasifali:12006051@cluster0.2vpfnb9.mongodb.net/test",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect("mongodb://127.0.0.1:27017/JWOC", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log("Database Not Connected !!!", err));
