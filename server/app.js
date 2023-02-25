e41979cc7c4e46d57380ac14edc23bd6feb121ba
Laxman Kohar <lkverma5422@gmail.com>
2023-02-25 18:13:15 +0530

    Pre-commit

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const cors = require("cors");
-require("./config/dbConfig");
+const cors = require('cors');
+require('./config/dbConfig');
 
 // Import Router
-const studentRouter = require("./routes/studentRoutes");
-const sampleRouter = require("./routes/sampleRoutes");
-const newStudentRouter = require("./routes/newStudentRoutes");
-const noticeRouter = require("./routes/noticeRoutes");
-const teacherRouter = require("./routes/teacherRoutes");
-const attendanceRouter = require("./routes/attendanceRoutes");
-const adminDatapostRouter = require("./routes/adminDataPostRoutes");
+const studentRouter = require('./routes/studentRoutes');
+const sampleRouter = require('./routes/sampleRoutes');
+const newStudentRouter = require('./routes/newStudentRoutes');
+
+const noticeRouter = require('./routes/noticeRoutes');
+const teacherRouter = require('./routes/teacherRoutes');
+const attendanceRouter = require('./routes/attendanceRoutes');
+const adminDatapostRouter = require('./routes/adminDataPostRoutes');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // Routes
-app.use("/v1/student/attendance", attendanceRouter);
-app.use("/v1/student", studentRouter);
-app.use("/v1/notice", noticeRouter);
-app.use("/v1/teacher", teacherRouter);
-app.use("/v1", sampleRouter);
-app.use("/v1/newstudent", newStudentRouter);
-app.use("/v1/admin", adminDatapostRouter);
+app.use('/v1/student/attendance', attendanceRouter);
+app.use('/v1/student', studentRouter);
+app.use('/v1/notice', noticeRouter);
+app.use('/v1/teacher', teacherRouter);
+app.use('/v1', sampleRouter);
+app.use('/v1/newstudent', newStudentRouter);
+app.use('/v1/admin', adminDatapostRouter);
 module.exports = app;
--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/JWOC', {
+mongoose.connect('mongodb://127.0.0.1:27017/JWOC', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
